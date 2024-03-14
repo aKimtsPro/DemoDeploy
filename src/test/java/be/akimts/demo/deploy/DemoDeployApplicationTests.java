9b6f659b54bba0a90d315cedbf6bdc8dbc4a9a1b
Alexandre Kimtsaris <alexandre.kimtsaris@bstorm.be>
2024-03-14 16:04:45 +0100

    test

--- src/test/java/be/akimts/demo/deploy/DemoDeployApplicationTests.java
+++ src/test/java/be/akimts/demo/deploy/DemoDeployApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class DemoDeployApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
